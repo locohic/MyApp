e89f30b2ac7420c5be2b88a0614de5994fcab8cd
LOIC <ahigora.loic@gmail.com>
2016-11-07 18:00:53 +0100

    Application Android

--- app/src/main/java/com/example/loic/myapplication/MainActivity.java
+++ app/src/main/java/com/example/loic/myapplication/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.loic.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView TV1 =(TextView) findViewById(R.id.TV1);
+        getString(R.string.view_text);
+        String now= DateUtils.formatDateTime(getApplicationContext(),(new Date()).getTime(), DateFormat.FULL);
+        TV1.setText(now);
     }
 }
