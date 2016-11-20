c83fe49f1a754436edc479bab0c210cb2f442997
LOIC <ahigora.loic@gmail.com>
2016-11-20 19:56:39 +0100

    appli and

--- app/src/main/java/com/example/loic/myapplication/MainActivity.java
+++ app/src/main/java/com/example/loic/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,13 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button VALIDERbutton = (Button) findViewById(R.id.VALIDERbutton);
-        Button ANNULERbutton = (Button) findViewById(R.id.ANNULERbutton);
+        Button InscButton = (Button) findViewById(R.id.Inscbutton);
+        Button ConnectButton = (Button) findViewById(R.id.ConnectButton);
         TextView TV1 = (TextView) findViewById(R.id.TV1);
         getString(R.string.view_text);
         String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
         TV1.setText(now);
-        VALIDERbutton.setOnClickListener(new View.OnClickListener() {
+        InscButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view_text) {
 
@@ -33,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
             ;
         });
-        ANNULERbutton.setOnClickListener(new View.OnClickListener() {
+        ConnectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view_text) {
 
@@ -41,6 +43,38 @@ public class MainActivity extends AppCompatActivity {
             };
         });
     };
-};
+    private void save(){
+        Toast.makeText(this,R.string.action_save,Toast.LENGTH_LONG).show();
+    }
+
+    private void delete(){
+        Toast.makeText(this,R.string.action_delete,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()){
+            case R.id.action_save:
+                save();
+                return true;
+            case R.id.action_delete:
+                delete();
+                return true;
+            case R.id.action_mode_close_button:
+                closeContextMenu();
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
 
 
