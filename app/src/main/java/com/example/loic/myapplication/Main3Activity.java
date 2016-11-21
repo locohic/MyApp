ad1226bbb7f41433292a78bc973b448302d9cb5e
LOIC <ahigora.loic@gmail.com>
2016-11-21 16:29:00 +0100

    appli and

--- app/src/main/java/com/example/loic/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/loic/myapplication/Main3Activity.java
@@ -3,7 +3,10 @@ package com.example.loic.myapplication;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.net.URL;
 
 import static com.example.loic.myapplication.GetBiersService.*;
 import static com.example.loic.myapplication.GetBiersService.startActionBiers;
@@ -15,7 +18,12 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         Button Get_Biers_Button = (Button) findViewById(R.id.Get_Biers_Button);
+        Get_Biers_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetBiersService.startActionBiers(getApplicationContext(),"" );
+            }
+            });
     }
-
 }
 
